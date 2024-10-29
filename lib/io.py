5a752a2130f098774adaa4a04776fe09aeb4a4b9
Tony <tonymeissner70@gmail.com>
2024-10-29 22:31:55 +0100

    Refactor: Add clear_dir function to remove files and directories in the output directory

--- lib/io.py
+++ lib/io.py
@@ -12,6 +12,7 @@ import os
 import json
 from pathlib import Path
 import cv2
+import shutil
 
 
 def load_image(image_name: str) -> cv2.typing.MatLike:
@@ -49,3 +50,12 @@ def write_json(dict_to_save: dict, filepath: str) -> None:
     """
     with open(str(filepath), "w") as f:
         json.dump(dict_to_save, f)
+
+
+def clear_dir(dir: Path) -> None:
+    """Clears all files and directories in the output directory."""
+    for item in dir.iterdir():
+        if item.is_file():
+            item.unlink()
+        elif item.is_dir():
+            shutil.rmtree(item)
