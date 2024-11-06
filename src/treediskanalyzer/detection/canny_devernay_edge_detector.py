bdf05735cbebdbbd1a85d9c80bc6cbdf744439aa
Tony <tonymeissner70@gmail.com>
2024-11-06 21:48:06 +0100

    fix: update package name from treeringanalyzer to treediskanalyzer in canny_devernay_edge_detector.py

--- src/treediskanalyzer/detection/canny_devernay_edge_detector.py
+++ src/treediskanalyzer/detection/canny_devernay_edge_detector.py
@@ -13,7 +13,7 @@ from ..config import config
 def get_devernay_path():
     """Get the absolute path to the devernay binary."""
     binary_path = pkg_resources.resource_filename(
-        "treeringanalyzer", os.path.join("externals", "devernay_1.0", "devernay.out")
+        "treediskanalyzer", os.path.join("externals", "devernay_1.0", "devernay.out")
     )
 
     # Ensure the file exists and is executable
